8e574c93e0c9587d7dc556fdedfd9b64a80b6576
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-12-14 12:42:24 +0000

    商品詳細画面完成

--- db/schema.rb
+++ db/schema.rb
@@ -94,11 +94,11 @@ ActiveRecord::Schema.define(version: 2022_12_11_122559) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.integer "genre_id"
-    t.string "name"
-    t.text "introduction"
-    t.integer "price"
-    t.string "is_active"
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
