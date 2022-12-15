bc092330611a49456183cacfec02706766a3f4f4
NatsumiKobayashi <115065917+natsumikobayashi@users.noreply.github.com>
2022-12-15 13:12:51 +0000

    会員編集画面作成中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,8 +9,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+  
+  private
+  def customer_params
+    params.recuire(:customer).permit(:last_name, :first_name, :last_name, :first_name_kana, :last_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
